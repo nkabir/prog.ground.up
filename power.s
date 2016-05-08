c9fbcea2185b36d5294e9fc340225f7df7780c64
Mohammed Asif <diraar@gmail.com>
2016-05-08 16:31:34 +0000

    support for power of zero

--- power.s
+++ power.s
@@ -27,7 +27,7 @@
 
 		# do third call
 		pushl %ebx
-        pushl $3
+        pushl $0
         pushl $3
         call power
         addl $8, %esp
@@ -42,15 +42,26 @@
 
 .type power,  @function
 	power:
-		# ?
+		# setup
 		pushl %ebp
 		movl %esp, %ebp
-		subl $4, %esp
+		subl $4, %esp		# create space for local var
 
 		# load params
-		movl 8(%ebp), %ebx
-		movl 12(%ebp), %ecx
+		movl 8(%ebp), %ebx		# base
+		movl 12(%ebp), %ecx		# power
+
+		# return 1 if power is 0
+		cmpl $0, %ecx
+		je power_zero
+
+		# store base into initial result
 		movl %ebx, -4(%ebp)
+		jmp power_loop_start
+
+	power_zero:
+		movl $1, -4(%ebp)
+		jmp end_power
 
 	power_loop_start:
 		# have we looped down to 1?
@@ -67,9 +78,9 @@
 		jmp power_loop_start
 
 	end_power:
-		movl -4(%ebp), %eax
-		movl %ebp, %esp
-		popl %ebp
-		ret
+		movl -4(%ebp), %eax		# put result into %eax
+		movl %ebp, %esp			# dispose loc vars
+		popl %ebp				# restore base pointer to prev value
+		ret						# pop return address and use it to return
 
 
