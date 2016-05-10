beb749fb1168058a1b2f441e2b56abc9513089ad
Mohammed Asif <diraar@gmail.com>
2016-05-10 15:56:30 +0000

    square function

---
+++ square.s
@@ -0,0 +1,25 @@
+.section .data
+.section .text
+
+.global _start
+_start:
+	pushl $9
+	call square
+	addl $4, %esp
+	movl $eax, $ebx
+	movl $1, %eax
+	int $0x80
+
+.type square, @function
+square:
+	pushl %ebp
+	movl %ebp, %esp
+
+	movl 8(%ebp), %eax
+	imull %eax, %eax
+
+	movl %esp, %ebp
+	popl %ebp
+	ret
+
+
